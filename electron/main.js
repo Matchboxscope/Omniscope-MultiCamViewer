23352ffcfd123fa513c50a7d957148293d04112f
beniroquai <bene.d@gmx.de>
2023-11-01 23:40:40 +0100

    Adding abbility to package the Electron APP

--- electron/main.js
+++ electron/main.js
@@ -1,10 +1,10 @@
-//npm run electron-start
 const { app, BrowserWindow } = require('electron');
+const { fork, spawn } = require('child_process');
 const path = require('path');
-const { fork } = require('child_process');
 
 let mainWindow;
 let serverProcess = null;
+let reactProcess = null;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -15,30 +15,38 @@ function createWindow() {
     }
   });
 
-  // Point to where your React app is hosted
-  mainWindow.loadURL('http://localhost:3000');
+  // The URL should be where your React app will be served from.
+  // This is the default create-react-app port.
+  
+  setTimeout(() => {
+    mainWindow.loadURL('http://localhost:3000');
+  }, 2000); // 10 seconds delay
 
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', () => {
     mainWindow = null;
   });
 }
 
-app.on('ready', () => {
+app.whenReady().then(() => {
   // Start the Node.js server
   serverProcess = fork(path.join(__dirname, '../server/server.js'));
+
+  // Start the React client app
+  reactProcess = spawn('npm', ['start'], { cwd: path.join(__dirname, '../client'), stdio: 'inherit' });
 
   createWindow();
 });
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('quit', () => {
-  // Terminate the server when closing the app
+  // Terminate the server and React app when closing the Electron app
   if (serverProcess) serverProcess.kill();
+  if (reactProcess) reactProcess.kill();
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
   if (mainWindow === null) createWindow();
 });
