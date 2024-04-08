d29312afa379543da4f33644fb088594662422e8
beniroquai <bene.d@gmx.de>
2024-04-08 23:46:10 +0200

    Adding code that could send frames via SPI slave

--- ESP32/OmniscopeFW/main/main.ino
+++ ESP32/OmniscopeFW/main/main.ino
@@ -1,172 +1,182 @@
-// https://github.com/hideakitai/ESP32DMASPI/blob/main/examples/transfer_big_data_in_the_background/transfer_big_data_in_the_background_slave/transfer_big_data_in_the_background_slave.ino
-#include <ESP32DMASPISlave.h>
-#include "helper.h"
-#define PIN_NUM_MOSI D10
-#define PIN_NUM_MISO D9
-#define PIN_NUM_CLK D8
-#define PIN_NUM_CS 21
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/spi_slave.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
 #include "esp_camera.h"
-#include "camera_pins.h"
-//  slave.begin(HSPI, PIN_NUM_CLK, PIN_NUM_MISO, PIN_NUM_MOSI, PIN_NUM_CS);  // alternative pins (CS, CLK, MISO, MOSI
+#include <esp_err.h>
+#include <driver/spi_slave.h>
+#include <nvs_flash.h>
+#include <driver/spi_slave.h>
 
-#include "helper.h"
+// ESP32Cam (AiThinker) PIN Map
+#define CAM_PIN_PWDN 32
+#define CAM_PIN_RESET -1 //software reset will be performed
+#define CAM_PIN_XCLK 0
+#define CAM_PIN_SIOD 26
+#define CAM_PIN_SIOC 27
+#define CAM_PIN_D7 35
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 39
+#define CAM_PIN_D4 36
+#define CAM_PIN_D3 21
+#define CAM_PIN_D2 19
+#define CAM_PIN_D1 18
+#define CAM_PIN_D0 5
+#define CAM_PIN_VSYNC 25
+#define CAM_PIN_HREF 23
+#define CAM_PIN_PCLK 22
 
-ESP32DMASPI::Slave slave;
+#define GPIO_HANDSHAKE 2
+#define GPIO_MOSI 12
+#define GPIO_MISO 13
+#define GPIO_SCLK 15
+#define GPIO_CS 14
 
-static constexpr size_t BUFFER_SIZE = 256; // should be multiple of 4
-static constexpr size_t QUEUE_SIZE = 1;
-uint8_t *dma_tx_buf;
-uint8_t *dma_rx_buf;
+#define CAM_WIDTH   320
+#define CAM_HEIGHT  240
 
-void setup()
-{
-    Serial.begin(115200);
-    initCamera();
-    delay(2000);
+// SPI transaction states
+#define STATE_WAITING_FOR_REQUEST         2u
+#define STATE_SENDING_RESPONSE            3u
 
-    // to use DMA buffer, use these methods to allocate buffer
-    dma_tx_buf = slave.allocDMABuffer(BUFFER_SIZE);
-    dma_rx_buf = slave.allocDMABuffer(BUFFER_SIZE);
+// Requests
+#define REQ_CAMERA_PIC              0x02u
 
-    slave.setDataMode(SPI_MODE0);          // default: SPI_MODE0
-    slave.setMaxTransferSize(BUFFER_SIZE); // default: 4092 bytes
-    slave.setQueueSize(QUEUE_SIZE);        // default: 1
+// Function prototypes
+static esp_err_t init_camera();
+void spi_task(void *pvParameters);
 
-    // begin() after setting
-    slave.begin(HSPI, PIN_NUM_CLK, PIN_NUM_MISO, PIN_NUM_MOSI, PIN_NUM_CS); // alternative pins (CS, CLK, MISO, MOSI
-
-    Serial.println("start spi slave");
+// SPI Callbacks
+void my_post_setup_cb(spi_slave_transaction_t *trans) {
+    WRITE_PERI_REG(GPIO_OUT_W1TS_REG, (1<<GPIO_HANDSHAKE));
 }
 
-void loop()
-{
+void my_post_trans_cb(spi_slave_transaction_t *trans) {
+    WRITE_PERI_REG(GPIO_OUT_W1TC_REG, (1<<GPIO_HANDSHAKE));
+}
 
-    // get image from camera
-    camera_fb_t *fb = esp_camera_fb_get();
-    size_t original_size = fb->len;
-    Serial.println("Original frame size: " + String(original_size));
-    Serial.println(esp_get_free_heap_size());
+// Camera configuration
+static camera_config_t camera_config = {
+    .pin_pwdn = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sscb_sda = CAM_PIN_SIOD,
+    .pin_sscb_scl = CAM_PIN_SIOC,
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+    .pixel_format = PIXFORMAT_GRAYSCALE,
+    .frame_size = FRAMESIZE_QVGA,
+    .jpeg_quality = 12,
+    .fb_count = 1
+};
+
+// Main application entry point
+void setup() {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(init_camera());
+
+    xTaskCreate(spi_task, "spi_task", 4096, NULL, 5, NULL);
+}
+
+void loop(){
+}
+
+static esp_err_t init_camera() {
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        ESP_LOGE("camera", "Camera Init Failed");
+        return err;
+    }
+    ESP_LOGI("camera", "Camera Init Succeeded");
+    return ESP_OK;
+}
+
+void spi_task(void *pvParameters) {
+    esp_err_t ret;
+
+    // Configure SPI bus
+    spi_bus_config_t buscfg = {
+        .mosi_io_num = GPIO_MOSI,
+        .miso_io_num = GPIO_MISO,
+        .sclk_io_num = GPIO_SCLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = CAM_WIDTH*CAM_HEIGHT
+    };
+
+    // Configure SPI slave interface
+    spi_slave_interface_config_t slvcfg;
+    slvcfg.mode = 1;
+    slvcfg.spics_io_num = GPIO_CS;
+    slvcfg.queue_size = 3;
+    slvcfg.flags = 0;
+    slvcfg.post_setup_cb = my_post_setup_cb;
+    slvcfg.post_trans_cb = my_post_trans_cb;
     
-    // Calculate the size of the new buffer
-    size_t padded_size = (original_size + 3) & ~3; // Round up to multiple of 4
+    // Handshake line configuration
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = (1<<GPIO_HANDSHAKE);
+    gpio_config(&io_conf);
 
-    // Allocate a new buffer
-    uint8_t *padded_data = new uint8_t[padded_size];
+    // Initialize SPI slave
+    ret = spi_slave_initialize(SPI2_HOST, &buscfg, &slvcfg, 1);
+    assert(ret == ESP_OK);
 
-    // Copy the original data
-    memcpy(padded_data, fb->buf, original_size);
-    esp_camera_fb_return(fb);
+    uint8_t *camera_frame = (uint8_t *)heap_caps_malloc(CAM_WIDTH*CAM_HEIGHT, MALLOC_CAP_DMA);
+    spi_slave_transaction_t t = { 0 };
+    uint8_t recvbuf[1]; // Buffer to receive request
 
-    // Pad the rest with zeros
-    if (padded_size > original_size)
-    {
-        memset(padded_data + original_size, 0, padded_size - original_size);
-    }
+    while (1) {
+        memset(recvbuf, 0, sizeof(recvbuf));
+        t.length = 8; // 8 bits
+        t.tx_buffer = NULL;
+        t.rx_buffer = recvbuf;
+        ret = spi_slave_transmit(SPI2_HOST, &t, portMAX_DELAY);
 
-    Serial.println("Padded frame size: " + String(padded_size));
-    Serial.println(esp_get_free_heap_size());
-    // send image to master -> MUST BE MUTIPLE OF 4!!!!
-    // initialize tx/rx buffers
-    if (slave.hasTransactionsCompletedAndAllResultsHandled())
-    {
-        // initialize tx/rx buffers
-        Serial.println("initialize tx/rx buffers");
-        initializeBuffers(dma_tx_buf, dma_rx_buf, padded_size, 0);
+        if (ret == ESP_OK && recvbuf[0] == REQ_CAMERA_PIC) {
+            // Capture the frame
+            camera_fb_t *pic = esp_camera_fb_get();
+            if (!pic) {
+                ESP_LOGE("camera", "Camera Capture Failed");
+                continue;
+            }
 
-        // queue transaction and trigger it right now
-        Serial.println("execute transaction in the background");
-        Serial.println(esp_get_free_heap_size());
-        slave.queue(padded_data, dma_rx_buf, padded_size);
-        slave.trigger();
+            // Prepare for sending the frame over SPI
+            memcpy(camera_frame, pic->buf, pic->len);
+            esp_camera_fb_return(pic);
 
-        Serial.println("wait for the completion of the queued transactions...");
-        Serial.println(esp_get_free_heap_size());
-    }
-
-    // if all transactions are completed and all results are ready, handle results
-    if (slave.hasTransactionsCompletedAndAllResultsReady(padded_size))
-    {
-        // process received data from slave
-        Serial.println("all queued transactions completed. start verifying received data from slave");
-
-        // get the oldeest transfer result
-        size_t received_bytes = slave.numBytesReceived();
-
-        Serial.println("received_bytes: " + String(received_bytes));
-        // verify and dump difference with received data
-        // NOTE: we need only 1st results (received_bytes[0])
-        if (verifyAndDumpDifference("slave", dma_tx_buf, BUFFER_SIZE, "master", dma_rx_buf, received_bytes))
-        {
-            Serial.println("successfully received expected data from master");
+            // Send the frame over SPI
+            t.length = 8 * CAM_WIDTH*CAM_HEIGHT; // bit length
+            t.tx_buffer = camera_frame;
+            t.rx_buffer = NULL;
+            ret = spi_slave_transmit(SPI1_HOST, &t, portMAX_DELAY);
+            if (ret != ESP_OK) {
+                ESP_LOGE("spi", "Failed to transmit frame");
+            }
         }
-        else
-        {
-            Serial.println("Unexpected difference found between master/slave data");
-        }
-    }
-    delay(200);
-}
-
-void initCamera()
-{
-    camera_config_t config;
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sscb_sda = SIOD_GPIO_NUM;
-    config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT_JPEG; // for streaming
-    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-    config.fb_location = CAMERA_FB_IN_PSRAM;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-
-    /*
-       FRAMESIZE_UXGA (1600 x 1200)
-      FRAMESIZE_QVGA (320 x 240)
-      FRAMESIZE_CIF (352 x 288)
-      FRAMESIZE_VGA (640 x 480)
-      FRAMESIZE_SVGA (800 x 600)
-      FRAMESIZE_XGA (1024 x 768)
-      FRAMESIZE_SXGA (1280 x 1024)
-    */
-    // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-    //                      for larger pre-allocated frame buffer.
-    if (psramFound())
-    {
-        Serial.println("PSRAM FOUND");
-        config.jpeg_quality = 10;
-        config.fb_count = 2;
-        config.grab_mode = CAMERA_GRAB_LATEST;
-        config.frame_size = FRAMESIZE_VGA; // iFRAMESIZE_UXGA;
-    }
-    else
-    {
-        log_d("NO PSRAM");
-        // Limit the frame size when PSRAM is not available
-        config.frame_size = FRAMESIZE_VGA;
-        config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-
-    // camera init
-    esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK)
-    {
-        log_e("Camera init failed with error 0x%x", err);
-        ESP.restart();
     }
 }
